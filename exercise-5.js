1276e5552d22791334d6147a3def83e78716f395
CallmeAkshit <akshit.b@gmail.com>
2021-01-31 15:55:00 +0530

    Update exercise-5.js

--- exercise-5.js
+++ exercise-5.js
@@ -1,7 +1,7 @@
 
 
 window.addEventListener('defer', () => {
-    const input=document.querySelector('#some-input');
+    const input=document.querySelector('#text-input-1');
     const box=document.querySelector('#color');
 
         input.addEventListener('input', event => {
